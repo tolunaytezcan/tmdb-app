7df0ed015104bc4a1185eea0f5157fa324b6763e
Tolunay Tezcan <tolunay.tezcan@innovance.com.tr>
2021-03-13 00:59:34 +0300

    Adding Footer to homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+import { Footer,Header } from './components';
+
 
 function App() {
   return (
     <div>
      <Header/>
+     <h2>Popüler Olanlar</h2>
+     <Footer/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import '../../logo.svg'
 
 function Header() {
     return (
-        <div className="body">
+        <div className="header">
             <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="The Movie Database (TMDb)" className="logo"></img>
         </div>
     )
