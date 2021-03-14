5544c4770a0c398d30d9594d860ffbd31511191e
Tolunay Tezcan <tolunay.tezcan@innovance.com.tr>
2021-03-14 18:36:15 +0300

    Deploy the movie db app

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -22,81 +22,84 @@ function Homepage({ title, fetchUrl }) {
 
   return (
     <div className="row">
-      <h2>{title}</h2>
+      <div className="display">
+        <h2>{title}</h2>
 
-      {title === "What's Popular" && (
-        <div className="navWrapper">
-          <Router>
-            <NavLink
-              to="/streaming"
-              onClick={() => {
-                setUrl(requests.fetctList);
-              }}
-              className="Link"
-              activeClassName="activeLink"
-            >
-              Streaming
-            </NavLink>
-            <NavLink
-              to="/on-tv"
-              onClick={() => {
-                setUrl(requests.fetchActionMovies);
-              }}
-              className="Link"
-              activeClassName="activeLink"
-            >
-              On TV
-            </NavLink>
-            <NavLink
-              to="/for-rent"
-              onClick={() => {
-                setUrl(requests.fetchComedyMovies);
-              }}
-              className="Link"
-              activeClassName="activeLink"
-            >
-              For Rent
-            </NavLink>
-            <NavLink
-              to="/in-theaters"
-              onClick={() => {
-                setUrl(requests.fetchTheater);
-              }}
-              className="Link"
-              activeClassName="activeLink"
-            >
-              In Theaters
-            </NavLink>
-          </Router>
-        </div>
-      )}
-
-      {title === "Free To Watch" && (
-        <div className="navWrapper">
-          <Router>
-            <NavLink
-              to="/movies"
-              onClick={() => {
-                setUrl(requests.fetctList);
-              }}
-              className="Link"
-              activeClassName="activeLink"
-            >
-              Movies
-            </NavLink>
-            <NavLink
-              to="/tv"
-              onClick={() => {
-                setUrl(requests.fetchActionMovies);
-              }}
-              className="Link"
-              activeClassName="activeLink"
-            >
-              TV
-            </NavLink>
-          </Router>
-        </div>
-      )}
+        {title === "What's Popular" && (
+          <div className="navWrapper">
+            <Router>
+              <NavLink
+                to="/"
+                exact={true}
+                onClick={() => {
+                  setUrl(requests.fetctList);
+                }}
+                className="Link"
+                activeClassName="activeLink"
+              >
+                Streaming
+              </NavLink>
+              <NavLink
+                to="/on-tv"
+                onClick={() => {
+                  setUrl(requests.fetchActionMovies);
+                }}
+                className="Link"
+                activeClassName="activeLink"
+              >
+                On TV
+              </NavLink>
+              <NavLink
+                to="/for-rent"
+                onClick={() => {
+                  setUrl(requests.fetchComedyMovies);
+                }}
+                className="Link"
+                activeClassName="activeLink"
+              >
+                For Rent
+              </NavLink>
+              <NavLink
+                to="/in-theaters"
+                onClick={() => {
+                  setUrl(requests.fetchTheater);
+                }}
+                className="Link"
+                activeClassName="activeLink"
+              >
+                In Theaters
+              </NavLink>
+            </Router>
+          </div>
+        )}
+        {title === "Free To Watch" && (
+          <div className="navWrapper">
+            <Router>
+              <NavLink
+                to="/"
+                exact={true}
+                onClick={() => {
+                  setUrl(requests.fetctList);
+                }}
+                className="Link"
+                activeClassName="activeLink"
+              >
+                Movies
+              </NavLink>
+              <NavLink
+                to="/tv"
+                onClick={() => {
+                  setUrl(requests.fetchActionMovies);
+                }}
+                className="Link"
+                activeClassName="activeLink"
+              >
+                TV
+              </NavLink>
+            </Router>
+          </div>
+        )}
+      </div>
 
       <Movie movies={movies}></Movie>
     </div>
--- src/components/Homepage/Movie.js
+++ src/components/Homepage/Movie.js
@@ -9,14 +9,15 @@ function Movie({ movies }) {
       {movies.map((movie) => (
         <div>
           <img
-            className={`image ${movie.vote_average * 10 <= 65 && "low"} ${
-              movie.vote_average * 10 > 65 && "mid"
-            } ${movie.vote_average * 10 > 80 && "high"}`}
-            src={`${baseImgUrl}/${movie.backdrop_path}`}
+            className={`image ${movie.vote_average <= 7 && "low"} ${
+              movie.vote_average > 7 && "mid"
+            } ${movie.vote_average > 8 && "high"}`}
+            src={`${baseImgUrl}/${movie.poster_path}`}
             alt={movie.name}
             key={movie.id}
           />
           <b>%{movie.vote_average * 10}</b>
+
           <br />
           <strong className="movie_name">{movie.original_title}</strong>
           <p className="release_date">{movie.release_date}</p>
